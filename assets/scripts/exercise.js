53bca57735da2f54df8b4003be5f1828f390553e
Malachi Barton <78632722+MBarton2579@users.noreply.github.com>
2024-01-22 20:38:00 +0000

    Updated sub pages with new menu

---
+++ assets/scripts/exercise.js
@@ -0,0 +1,28 @@
+let searchButton = document.querySelector('.searchButton');
+let closeButton = document.querySelector('.closeButton');
+let searchBox = document.querySelector('.searchBox');
+let menuButton = document.querySelector('.menuButton')
+let navigation = document.querySelector('.navigation')
+let header = document.querySelector('header')
+
+searchButton.onclick = function(){
+    searchBox.classList.add('active');
+    closeButton.classList.add('active');
+    searchButton.classList.add('active');
+    menuButton.classList.add('hide');
+    header.classList.remove('open');
+}
+
+closeButton.onclick = function(){
+    searchBox.classList.remove('active');
+    closeButton.classList.remove('active');
+    searchButton.classList.remove('active');
+    menuButton.classList.remove('hide');
+}
+
+menuButton.onclick = function(){
+    header.classList.toggle('open');
+    searchBox.classList.remove('active');
+    closeButton.classList.remove('active');
+    searchButton.classList.remove('active');
+}
